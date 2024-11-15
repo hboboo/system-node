7afdf18391e91d23df545ebae336ee880f940691
hb <515870153@qq.com>
2024-11-15 19:48:10 +0800

    2024年11月15日

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//跨域中间件
+const cors = require('cors')
+app.use(cors())
+
+//解析表单中间件
+let bodyParser = require('body-parser')
+// 为false时不能给嵌套对象正常解析
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// 把json转变成js对象
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+  console.log(`已连接 ${port}`)
+})
